4327c6a49efdd6f1005d0a095901e98697de3060
www-data <www-data@instance-20251109-211644.us-central1-a.c.spry-device-474318-c2.internal>
2025-11-12 19:08:02 +0000

    Admin sync: main → goldy_database at 2025-11-12 19:08:02

--- dumps/main_to_goldy.sql
+++ dumps/main_to_goldy.sql
@@ -35,6 +35,30 @@ LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `sync_data_php`
+--
+
+DROP TABLE IF EXISTS `sync_data_php`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sync_data_php` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `permissions` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sync_data_php`
+--
+
+LOCK TABLES `sync_data_php` WRITE;
+/*!40000 ALTER TABLE `sync_data_php` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sync_data_php` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -45,4 +69,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-12 19:02:08
+-- Dump completed on 2025-11-12 19:08:02
