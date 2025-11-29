16d8857a9bfb6b440f7c054b40ab5721af7986e1
www-data <www-data@instance-20251109-211644.us-central1-a.c.spry-device-474318-c2.internal>
2025-11-29 19:12:56 +0000

    Admin sync: main → tejas_database at 2025-11-29 19:12:56

---
+++ dumps/backup_tejas_database_2025-11-29_19-12-56.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
+--
+-- Host: localhost    Database: tejas_database
+-- ------------------------------------------------------
+-- Server version	8.0.44-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sync_data_php`
+--
+
+DROP TABLE IF EXISTS `sync_data_php`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sync_data_php` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `permissions` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sync_data_php`
+--
+
+LOCK TABLES `sync_data_php` WRITE;
+/*!40000 ALTER TABLE `sync_data_php` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sync_data_php` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `full_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country_code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '1' COMMENT '1 = active, 9 = disabled',
+  `kyc_status` int NOT NULL DEFAULT '0' COMMENT '1=approved, 0=not done, 2=pending, 3=expired, 4=disabled',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `idx_users_email` (`email`),
+  KEY `idx_users_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('2a30bec3-c49b-11f0-b299-42010a800002','goldy','data','goldyg8321@gmail.com','IN','2345678900349','$2b$10$4YWYaect41JJ5KwERxrpQuNrdWx61FXNbNfTpxnJ1DsmkY/xJeZ.e','2025-11-18 16:25:15','2025-11-18 16:25:15',NULL,1,0),('47f1e447-c498-11f0-b299-42010a800002','Trial ','Acc','tejas@saas.in','US','9387892902','$2b$10$vK04VSzlq551Y6J3FcBISOsmzzi3m5E0i7G5uvnCpVHEUcUJp.ndi','2025-11-18 16:04:36','2025-11-18 16:04:36',NULL,1,0),('679cbf68-c49c-11f0-b299-42010a800002','Jon Snow','Acme In','walephysics@gmail.com','IN','9876543210','$2b$10$mtaQPq8HoaRW4eHcKXk40OyAYDhCLP2V7iLilPYHIO0lHUILznk8e','2025-11-18 16:34:08','2025-11-18 16:34:08',NULL,1,0),('9f49b8e9-c199-11f0-b299-42010a800002','AC','SDNJ','singhtejas0001@gmail.com','US','0987270288','$2b$10$Sb3YbrZxf7O8wVhyu9ZEAuaOXeGqNq0ZrKeQpKIa2ALcwWmLIFyz.','2025-11-14 20:36:39','2025-11-15 22:49:40',NULL,1,0),('d800ef26-c199-11f0-b299-42010a800002','AC','SDNJ','tejas@gmail.com','US','0987270288','$2b$10$qGaukT1UVEyrBk2S3Bg89eAKQWdpzYkSuoBgMtvPrOcPZF3NtE8u2','2025-11-14 20:38:14','2025-11-14 20:38:14',NULL,1,0),('f3cfc147-c19e-11f0-b299-42010a800002','Trial ','Acc','trial@t.com','IN','9387892902','$2b$10$T.gZIeMz76YFbUkiHt6MG.EFXXVue3Nn0qZCQ8ww56A9lOyrjyxWi','2025-11-14 21:14:48','2025-11-15 22:50:00',NULL,1,9);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-29 19:12:56
