8dc1bc0469152ebb5151e1e1b24fab070c6ed77d
www-data <www-data@instance-20251129-193300.us-central1-c.c.seismic-bucksaw-479719-d2.internal>
2025-11-29 20:59:17 +0000

    Admin sync: main → goldy_database at 2025-11-29 20:59:17

--- dumps/backup_goldy_database_2025-11-29_20-59-17.sql
+++ dumps/backup_goldy_database_2025-11-29_20-59-17.sql
@@ -1,7 +1,6 @@
-mysqldump: Got error: 1049: Unknown database 'main_database' when selecting the database
 -- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
 --
--- Host: localhost    Database: main_database
+-- Host: localhost    Database: goldy_database
 -- ------------------------------------------------------
 -- Server version	8.0.44-0ubuntu0.24.04.1
 
@@ -15,3 +14,14 @@ mysqldump: Got error: 1049: Unknown database 'main_database' when selecting the 
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-29 20:59:17
